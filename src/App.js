7a350f5cfd033e1ca3ed4eec8b457349e564e23e
Bobby Jones Jr <jones9682@yahoo.com>
2020-10-08 16:29:45 -0400

    added routes for pages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import './App.css';
+
 import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
 
 
 class App extends React.Component {
@@ -49,6 +53,10 @@ class App extends React.Component {
               </Nav>
             </Navbar.Collapse>
           </Navbar>
+
+    <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.subTitle} text={this.state.home.text} />} />
+    <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
+    <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
 
           <Footer />
 
